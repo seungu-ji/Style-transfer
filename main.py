d04e1eb9d143d22a52e64524972365c84174b4b5
viriditass <jiseungwoo0210@gmail.com>
2021-04-26 14:32:48 +0900

    add decoder, style transfer function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import argparse
+import time
 from PIL import Image
 
 from model import *
 from utils import *
+# wct
+from wct import *
 
 import torch
 import torch.nn as nn
@@ -11,9 +14,10 @@ import torchvision
 from torchvision import transforms
 from torch.autograd import Variable
 
+import torchvision.transforms.functional as tf
 
 def main():
-    parser = argparse.ArgumentParser(description='Image Style Transfer Using Convolutional Neural Networks',
+    parser = argparse.ArgumentParser(description='Image Style Transfer Using Convolutional Neural Networks && Stable and Controllable Neural Texture Synthesis and Style Transfer Using Histogram Losses',
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--content_image', default='./datasets/content/Tuebingen_Neckarfront.jpg', type=str, dest='content_image')
@@ -82,6 +86,7 @@ def main():
     # loss setting
     loss_layer = style_layers + content_layer
     fn_loss = [GramMSELoss()] * len(style_layers) + [nn.MSELoss()] * len(content_layer)
+
     if args.histogram_steps is not None:
       fn_loss += [HistogramLoss(num_steps=150, cuda=True)]
     fn_loss = [loss.to(device) for loss in fn_loss]
@@ -128,5 +133,61 @@ def main():
     gcf().set_size_inches(10,10)
     """
 
+def wct_main():
+    parser = argparse.ArgumentParser(description='Universal Style Transfer via Feature Transforms',
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--content_image', default='./datasets/content/Tuebingen_Neckarfront.jpg', type=str, dest='content_image')
+    parser.add_argument('--style_image', default='./datasets/style/vangogh_starry_night.jpg', type=str, dest='style_image')
+    #parser.add_argument('--weight_path', default='./weights/vgg_conv.pth', type=str, dest='weight_path')
+    parser.add_argument('--alpha', type=float,default=1.0, help='hyperparameter to blend wct feature and content feature')
+    parser.add_argument('--img_size', default=512, type=int, dest='img_size')
+
+    args = parser.parse_args()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    encoder1 = './wct_weights/vgg_normalised_conv1_1.pth'
+    encoder2 = './wct_weights/vgg_normalised_conv2_1.pth'
+    encoder3 = './wct_weights/vgg_normalised_conv3_1.pth'
+    encoder4 = './wct_weights/vgg_normalised_conv4_1.pth'
+    encoder5 = './wct_weights/vgg_normalised_conv5_1.pth'
+    decoder1 = './wct_weights/feature_invertor_conv1_1.pth'
+    decoder2 = './wct_weights/feature_invertor_conv2_1.pth'
+    decoder3 = './wct_weights/feature_invertor_conv3_1.pth'
+    decoder4 = './wct_weights/feature_invertor_conv4_1.pth'
+    decoder5 = './wct_weights/feature_invertor_conv5_1.pth'
+
+    paths = encoder1, encoder2, encoder3, encoder4, encoder5, decoder1, decoder2, decoder3, decoder4, decoder5
+    wct = WCT(paths)
+
+    content_image = Image.open(args.content_image).resize((args.img_size, args.img_size))
+    content_image = tf.to_tensor(content_image)
+    content_image.unsqueeze_(0)
+
+    style_image = Image.open(args.style_image).resize((args.img_size, args.img_size))
+    style_image = tf.to_tensor(style_image)
+    style_image.unsqueeze_(0)
+    
+    csf = torch.Tensor()
+
+    cimg = Variable(content_image, volatile=True)
+    simg = Variable(style_image, volatile=True)
+    csf = Variable(csf)
+
+    cimg = cimg.to(device)
+    simg = simg.to(device)
+    csf = csf.to(device)
+    wct.to(device)
+
+    # style transfer
+    start_time = time.time()
+    wct_style_transfer(wct, args.alpha, cimg, simg, csf)
+    
+    end_time = time.time()
+    print('Start to End Time: %f' % (end_time - start_time))  
+
+
 if __name__ == '__main__':
-    main()
+    #main()
+    wct_main()
--- wct.py
+++ wct.py
@@ -4,29 +4,149 @@ import torch
 import torch.nn as nn
 
 
+def wct_style_transfer(wct, alpha, content_image, style_image, csf):
+    sf5 = wct.e5(style_image)
+    cf5 = wct.e5(content_image)
+    sf5 = sf5.data.cpu().squeeze(0)
+    cf5 = cf5.data.cpu().squeeze(0)
+    csf5 = wct.transform(cf5,sf5,csf,alpha)
+    im5 = wct.d5(csf5)
+
+    sf4 = wct.e4(style_image)
+    cf4 = wct.e4(im5)
+    sf4 = sf4.data.cpu().squeeze(0)
+    cf4 = cf4.data.cpu().squeeze(0)
+    csf4 = wct.transform(cf4,sf4,csf,alpha)
+    im4 = wct.d4(csf4)
+
+    sf3 = wct.e3(style_image)
+    cf3 = wct.e3(im4)
+    sf3 = sf3.data.cpu().squeeze(0)
+    cf3 = cf3.data.cpu().squeeze(0)
+    csf3 = wct.transform(cf3,sf3,csf,alpha)
+    im3 = wct.d3(csf3)
+
+    sf2 = wct.e2(style_image)
+    cf2 = wct.e2(im3)
+    sf2 = sf2.data.cpu().squeeze(0)
+    cf2 = cf2.data.cpu().squeeze(0)
+    csf2 = wct.transform(cf2,sf2,csf,alpha)
+    im2 = wct.d2(csf2)
+
+    sf1 = wct.e1(style_image)
+    cf1 = wct.e1(im2)
+    sf1 = sf1.data.cpu().squeeze(0)
+    cf1 = cf1.data.cpu().squeeze(0)
+    csf1 = wct.transform(cf1,sf1,csf,alpha)
+    im1 = wct.d1(csf1)
+
+    torch.utils.save_image(im1.data.cpu().float(), 'output.jpg')
+
+    return
+
 ## Whitening and Coloring Transform
 class WCT(nn.Module):
     def __init__(self, paths):
         super(WCT, self).__init__()
         encoder_path1, encoder_path2, encoder_path3, encoder_path4, encoder_path5, decoder_path1, decoder_path2, decoder_path3, decoder_path4, decoder_path5 = paths
 
+        ## Load pre-trained network
+        # load encoder
         encoder1_param = torch.load(encoder_path1)
-        encoder2_param = torch.load(encoder_path2)        
+        encoder2_param = torch.load(encoder_path2)
         encoder3_param = torch.load(encoder_path3)
-        encoder4_param = torch.load(encoder_path4)        
+        encoder4_param = torch.load(encoder_path4)
         encoder5_param = torch.load(encoder_path5)
+        # load decoder
         decoder1_param = torch.load(decoder_path1)
         decoder2_param = torch.load(decoder_path2)
         decoder3_param = torch.load(decoder_path3)
         decoder4_param = torch.load(decoder_path4)
         decoder5_param = torch.load(decoder_path5)
 
-    def whiten_and_color(self, c, s):
-        pass
+        self.e1 = encoder1(encoder1_param)
+        self.e2 = encoder1(encoder2_param)
+        self.e3 = encoder1(encoder3_param)
+        self.e4 = encoder1(encoder4_param)
+        self.e5 = encoder1(encoder5_param)
+
+        self.d1 = decoder1(decoder1_param)
+        self.d2 = decoder2(decoder2_param)
+        self.d3 = decoder3(decoder3_param)
+        self.d4 = decoder4(decoder4_param)
+        self.d5 = decoder5(decoder5_param)
+
+    # content feature, style feature
+    def whiten_and_color(self, cf, sf):
+        # content size
+        c_size = cf.size()
+        # channels x (height x width)
+        # content mean
+        c_mean = torch.mean(cf,1)
+        c_mean = c_mean.unsqueeze(1).expand_as(cf)
+        cf = cf - c_mean
+        
+        # content conv
+        c_conv = torch.mm(cf, cf.t()).div(c_size[1]-1) + torch.eye(c_size[0]).double()
+        #c_u, c_e, c_v = torch.linalg.svd(c_conv,some=False)
+        c_u, c_e, c_v = torch.svd(c_conv, some=False)
+
+        k_c = c_size[0]
+        for i in range(c_size[0]):
+            if c_e[i] < 0.00001:
+                k_c = i
+                break
+        
+
+        # style size
+        s_size = sf.size()
+        # style mean
+        s_mean = torch.mean(sf,1)
+        sf = sf - s_mean.unsqueeze(1).expand_as(sf)
+
+        # style conv
+        s_conv = torch.mm(sf, sf.t()).div(s_size[1]-1)
+        s_u, s_e, s_v = torch.svd(s_conv, some=False) #torch.linalg.svd(s_conv, some=False)
+
+        k_s = s_size[0]
+        for i in range(s_size[0]):
+            if s_e[i] < 0.00001:
+                k_s = i
+                break
 
 
-# ReflectionPad2d - Load weight for Conv2d - ReLU
-class load_conv(nn.Module):
+        c_d = (c_e[0:k_c]).pow(-0.5)
+        step1 = torch.mm(c_v[:,0:k_c], torch.diag(c_d))
+        step2 = torch.mm(step1, (c_v[:,0:k_c].t()))
+        whiten_cf = torch.mm(step2, cf)
+
+        s_d = (s_e[0:k_s]).pow(0.5)
+        target = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s], torch.diag(s_d)), (s_v[:,0:k_s].t())), whiten_cf)
+        target = target + s_mean.unsqueeze(1).expand_as(target)
+
+        return target
+        
+    def transform(self, cf, sf, csf, alpha):
+        cf = cf.double()
+        sf = sf.double()
+
+        cc, cw, ch = cf.size(0), cf.size(1), cf.size(2)
+        _, sw, sh = sf.size(0), sf.size(1), sf.size(2)
+        cfview = cf.view(cc, -1)
+        sfview = sf.view(cc, -1)
+
+        target = self.whiten_and_color(cfview, sfview)
+        target = target.view_as(cf)
+        ccsf = alpha *  target + (1.0 - alpha) * cf
+        ccsf = ccsf.float().unsqueeze(0)
+
+        with torch.no_grad():
+            csf.resize_(ccsf.size()).copy_(ccsf)
+
+        return csf
+
+## ReflectionPad2d - Load weight for Conv2d - ReLU
+class RCR2d(nn.Module):
     def __init__(self, vgg, num_param, in_channels, out_channels, kernel_size=3, stride=1, padding=1, reflect=True, relu=True):
         super().__init__()
         
@@ -41,7 +161,7 @@ class load_conv(nn.Module):
         layers += [self.conv]
 
         if relu:
-            layers += nn.ReLU(inplace=True)
+            layers += [nn.ReLU(inplace=True)]
         
         self.layers = nn.Sequential(*layers)
 
@@ -50,12 +170,13 @@ class load_conv(nn.Module):
         return self.layers(x)
 
 
+## Encoder
 class encoder1(nn.Module):
     def __init__(self, vgg):
         super(encoder1, self).__init__()
-        self.conv1 = load_conv(vgg, 0, 3, 3, 1, 1, 0, False, False)
+        self.conv1 = RCR2d(vgg, 0, 3, 3, 1, 1, 0, False, False)
 
-        self.conv2 = load_conv(vgg, 2, 3, 64, 3, 1, 0, True, True)
+        self.conv2 = RCR2d(vgg, 2, 3, 64, 3, 1, 0, True, True)
 
 
     def forward(self, x):
@@ -67,16 +188,16 @@ class encoder1(nn.Module):
 class encoder2(nn.Module):
     def __init__(self, vgg):
         super(encoder2, self).__init__()
-        self.conv1 = load_conv(vgg, 0, 3, 3, 1, 1, 0, False, False)
+        self.conv1 = RCR2d(vgg, 0, 3, 3, 1, 1, 0, False, False)
 
-        self.conv2 = load_conv(vgg, 2, 3, 64, 3, 1, 0, True, True)
+        self.conv2 = RCR2d(vgg, 2, 3, 64, 3, 1, 0, True, True)
 
-        self.conv3 = load_conv(vgg, 5, 64, 64, 3, 1, 0, True, True)
+        self.conv3 = RCR2d(vgg, 5, 64, 64, 3, 1, 0, True, True)
 
         # 112 x 112
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
         
-        self.conv4 = load_conv(vgg, 9, 64, 128, 3, 1, 0, True, True)
+        self.conv4 = RCR2d(vgg, 9, 64, 128, 3, 1, 0, True, True)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -92,23 +213,23 @@ class encoder2(nn.Module):
 class encoder3(nn.Module):
     def __init__(self, vgg):
         super(encoder3, self).__init__()
-        self.conv1 = load_conv(vgg, 0, 3, 3, 1, 1, 0, False, False)
+        self.conv1 = RCR2d(vgg, 0, 3, 3, 1, 1, 0, False, False)
 
-        self.conv2 = load_conv(vgg, 2, 3, 64, 3, 1, 0, True, True)
+        self.conv2 = RCR2d(vgg, 2, 3, 64, 3, 1, 0, True, True)
 
-        self.conv3 = load_conv(vgg, 5, 64, 64, 3, 1, 0, True, True)
+        self.conv3 = RCR2d(vgg, 5, 64, 64, 3, 1, 0, True, True)
 
         # 112 x 112
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv4 = load_conv(vgg, 9, 64, 128, 3, 1, 0, True, True)
+        self.conv4 = RCR2d(vgg, 9, 64, 128, 3, 1, 0, True, True)
 
-        self.conv5 = load_conv(vgg, 12, 128, 128, 3, 1, 0, True, True)
+        self.conv5 = RCR2d(vgg, 12, 128, 128, 3, 1, 0, True, True)
 
         # 56 x 56
         self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv6 = load_conv(vgg, 16, 128, 256, 3, 1, 0, True, True)
+        self.conv6 = RCR2d(vgg, 16, 128, 256, 3, 1, 0, True, True)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -129,34 +250,34 @@ class encoder3(nn.Module):
 class encoder4(nn.Module):
     def __init__(self, vgg):
         super(encoder4, self).__init__()
-        self.conv1 = load_conv(vgg, 0, 3, 3, 1, 1, 0, False, False)
+        self.conv1 = RCR2d(vgg, 0, 3, 3, 1, 1, 0, False, False)
 
-        self.conv2 = load_conv(vgg, 2, 3, 64, 3, 1, 0, True, True)
+        self.conv2 = RCR2d(vgg, 2, 3, 64, 3, 1, 0, True, True)
 
-        self.conv3 = load_conv(vgg, 5, 64, 64, 3, 1, 0, True, True)
+        self.conv3 = RCR2d(vgg, 5, 64, 64, 3, 1, 0, True, True)
 
         # 112 x 112
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv4 = load_conv(vgg, 9, 64, 128, 3, 1, 0, True, True)
+        self.conv4 = RCR2d(vgg, 9, 64, 128, 3, 1, 0, True, True)
 
-        self.conv5 = load_conv(vgg, 12, 128, 128, 3, 1, 0, True, True)
+        self.conv5 = RCR2d(vgg, 12, 128, 128, 3, 1, 0, True, True)
 
         # 56 x 56
         self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv6 = load_conv(vgg, 16, 128, 256, 3, 1, 0, True, True)
+        self.conv6 = RCR2d(vgg, 16, 128, 256, 3, 1, 0, True, True)
 
-        self.conv7 = load_conv(vgg, 19, 256, 256, 3, 1, 0, True, True)
+        self.conv7 = RCR2d(vgg, 19, 256, 256, 3, 1, 0, True, True)
 
-        self.conv8 = load_conv(vgg, 22, 256, 256, 3, 1, 0, True, True)
+        self.conv8 = RCR2d(vgg, 22, 256, 256, 3, 1, 0, True, True)
         
-        self.conv9 = load_conv(vgg, 25, 256, 256, 3, 1, 0, True, True)
+        self.conv9 = RCR2d(vgg, 25, 256, 256, 3, 1, 0, True, True)
 
         # 28 x 28
         self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv10 = load_conv(vgg, 29, 256, 512, 3, 1, 0, True, True)
+        self.conv10 = RCR2d(vgg, 29, 256, 512, 3, 1, 0, True, True)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -184,45 +305,45 @@ class encoder4(nn.Module):
 class encoder5(nn.Module):
     def __init__(self, vgg):
         super(encoder5, self).__init__()
-        self.conv1 = load_conv(vgg, 0, 3, 3, 1, 1, 0, False, False)
+        self.conv1 = RCR2d(vgg, 0, 3, 3, 1, 1, 0, False, False)
 
-        self.conv2 = load_conv(vgg, 2, 3, 64, 3, 1, 0, True, True)
+        self.conv2 = RCR2d(vgg, 2, 3, 64, 3, 1, 0, True, True)
 
-        self.conv3 = load_conv(vgg, 5, 64, 64, 3, 1, 0, True, True)
+        self.conv3 = RCR2d(vgg, 5, 64, 64, 3, 1, 0, True, True)
 
         # 112 x 112
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv4 = load_conv(vgg, 9, 64, 128, 3, 1, 0, True, True)
+        self.conv4 = RCR2d(vgg, 9, 64, 128, 3, 1, 0, True, True)
 
-        self.conv5 = load_conv(vgg, 12, 128, 128, 3, 1, 0, True, True)
+        self.conv5 = RCR2d(vgg, 12, 128, 128, 3, 1, 0, True, True)
 
         # 56 x 56
         self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv6 = load_conv(vgg, 16, 128, 256, 3, 1, 0, True, True)
+        self.conv6 = RCR2d(vgg, 16, 128, 256, 3, 1, 0, True, True)
 
-        self.conv7 = load_conv(vgg, 19, 256, 256, 3, 1, 0, True, True)
+        self.conv7 = RCR2d(vgg, 19, 256, 256, 3, 1, 0, True, True)
 
-        self.conv8 = load_conv(vgg, 22, 256, 256, 3, 1, 0, True, True)
+        self.conv8 = RCR2d(vgg, 22, 256, 256, 3, 1, 0, True, True)
         
-        self.conv9 = load_conv(vgg, 25, 256, 256, 3, 1, 0, True, True)
+        self.conv9 = RCR2d(vgg, 25, 256, 256, 3, 1, 0, True, True)
 
         # 28 x 28
         self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv10 = load_conv(vgg, 29, 256, 512, 3, 1, 0, True, True)
+        self.conv10 = RCR2d(vgg, 29, 256, 512, 3, 1, 0, True, True)
 
-        self.conv11 = load_conv(vgg, 32, 512, 512, 3, 1, 0, True, True)
+        self.conv11 = RCR2d(vgg, 32, 512, 512, 3, 1, 0, True, True)
 
-        self.conv12 = load_conv(vgg, 35, 512, 512, 3, 1, 0, True, True)
+        self.conv12 = RCR2d(vgg, 35, 512, 512, 3, 1, 0, True, True)
 
-        self.conv13 = load_conv(vgg, 38, 512, 512, 3, 1, 0, True, True)
+        self.conv13 = RCR2d(vgg, 38, 512, 512, 3, 1, 0, True, True)
 
         # 14 x 14
         self.maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        self.conv14 = load_conv(vgg, 42, 512, 512, 3, 1, 0, True, True)
+        self.conv14 = RCR2d(vgg, 42, 512, 512, 3, 1, 0, True, True)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -255,10 +376,11 @@ class encoder5(nn.Module):
         return x
 
 
+## Decoder
 class decoder1(nn.Module):
     def __init__(self, dvgg):
         super(decoder1, self).__init__()
-        self.conv3 = load_conv(dvgg, 1, 64, 3, 3, 1, 0, True, False)
+        self.conv3 = RCR2d(dvgg, 1, 64, 3, 3, 1, 0, True, False)
 
     def forward(self, x):
         x = self.conv3(x)
@@ -269,14 +391,14 @@ class decoder2(nn.Module):
     def __init__(self, dvgg):
         super(decoder2, self).__init__()
         # 112 x 112
-        self.conv5 = load_conv(dvgg, 1, 128, 64, 3, 1, 0, True, True)
+        self.conv5 = RCR2d(dvgg, 1, 128, 64, 3, 1, 0, True, True)
         
         # 224 x 224
         self.unpool1 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.conv6 = load_conv(dvgg, 5, 64, 64, 3, 1, 0, True, True)
+        self.conv6 = RCR2d(dvgg, 5, 64, 64, 3, 1, 0, True, True)
 
-        self.conv7 = load_conv(dvgg, 8, 64, 3, 3, 1, 0, True, False)
+        self.conv7 = RCR2d(dvgg, 8, 64, 3, 3, 1, 0, True, False)
 
     def forward(self, x):
         x = self.conv5(x)
@@ -292,9 +414,153 @@ class decoder3(nn.Module):
     def __init__(self, dvgg):
         super(decoder3, self).__init__()
         # 56 x 56
-        self.conv7 = load_conv(dvgg, 1, 256, 128, 3, 1, 0, True, True)
+        self.conv7 = RCR2d(dvgg, 1, 256, 128, 3, 1, 0, True, True)
 
         # 112 x 112
         self.unpool1 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.conv8 = load_conv(dvgg, 5, 128, 128, 3, 1, 0, True, True)
+        self.conv8 = RCR2d(dvgg, 5, 128, 128, 3, 1, 0, True, True)
+
+        self.conv9 = RCR2d(dvgg, 8, 128, 64, 3, 1, 0, True, True)
+
+        # 224 x 224
+        self.unpool2 = nn.UpsamplingNearest2d(scale_factor=2)
+
+        self.conv10 = RCR2d(dvgg, 12, 64, 64, 3, 1, 0, True, True)
+
+        self.conv11 = RCR2d(dvgg, 15, 64, 3, 3, 1, 0, True, False)
+
+    def forward(self, x):
+        x = self.conv7(x)
+
+        x = self.unpool1(x)
+
+        x = self.conv8(x)
+        x = self.conv9(x)
+
+        x = self.unpool2(x)
+
+        x = self.conv10(x)
+        x = self.conv11(x)
+
+        return x
+
+class decoder4(nn.Module):
+    def __init__(self, dvgg):
+        super(decoder4, self).__init__()
+        # 28 x 28
+        self.conv11 = RCR2d(dvgg, 1, 512, 256, 3, 1, 0, True, True)
+
+        self.unpool1 = nn.UpsamplingNearest2d(scale_factor=2)
+
+        self.conv12 = RCR2d(dvgg, 5, 256, 256, 3, 1, 0, True, True)
+
+        self.conv13 = RCR2d(dvgg, 8, 256, 256, 3, 1, 0, True, True)
+
+        self.conv14 = RCR2d(dvgg, 11, 256, 256, 3, 1, 0, True, True)
+
+        self.conv15 = RCR2d(dvgg, 14, 256, 128, 3, 1, 0, True, True)
+
+        self.unpool2 = nn.UpsamplingNearest2d(scale_factor=2)
+
+        self.conv16 = RCR2d(dvgg, 18, 128, 128, 3, 1, 0, True, True)
+
+        self.conv17 = RCR2d(dvgg, 21, 128, 64, 3, 1, 0, True, True)
+
+        self.unpool3 = nn.UpsamplingNearest2d(scale_factor=2)
+
+        self.conv18 = RCR2d(dvgg, 25, 64, 64, 3, 1, 0, True, True)
+
+        self.conv19 = RCR2d(dvgg, 28, 64, 3, 3, 1, 0, True, False)
+
+    def forward(self, x):
+        x = self.conv11(x)
+
+        x = self.unpool1(x)
+
+        x = self.conv12(x)
+        x = self.conv13(x)
+        x = self.conv14(x)
+        x = self.conv15(x)
+
+        x = self.unpool2(x)
+
+        x = self.conv16(x)
+        x = self.conv17(x)
+
+        x = self.unpool3(x)
+
+        x = self.conv18(x)
+        x = self.conv19(x)
+
+        return x
+
+class decoder5(nn.Module):
+    def __init__(self, dvgg):
+        super(decoder5, self).__init__()
+        #
+        self.conv15 = RCR2d(dvgg, 1, 512, 512, 3, 1, 0, True, True)
+
+        self.unpool1 = nn.UpsamplingNearest2d(scale_factor=2)
+        
+        self.conv16 = RCR2d(dvgg, 5, 512, 512, 3, 1, 0, True, True)
+        
+        self.conv17 = RCR2d(dvgg, 8, 512, 512, 3, 1, 0, True, True)
+        
+        self.conv18 = RCR2d(dvgg, 11, 512, 512, 3, 1, 0, True, True)
+        
+        self.conv19 = RCR2d(dvgg, 14, 512, 256, 3, 1, 0, True, True)
+
+        #
+        self.unpool2 = nn.UpsamplingNearest2d(scale_factor=2)
+        
+        self.conv20 = RCR2d(dvgg, 18, 256, 256, 3, 1, 0, True, True)
+        
+        self.conv21 = RCR2d(dvgg, 21, 256, 256, 3, 1, 0, True, True)
+        
+        self.conv22 = RCR2d(dvgg, 24, 256, 256, 3, 1, 0, True, True)
+        
+        self.conv23 = RCR2d(dvgg, 27, 256, 128, 3, 1, 0, True, True)
+
+        #
+        self.unpool3 = nn.UpsamplingNearest2d(scale_factor=2)
+        
+        self.conv24 = RCR2d(dvgg, 31, 128, 128, 3, 1, 0, True, True)
+        
+        self.conv25 = RCR2d(dvgg, 34, 128, 64, 3, 1, 0, True, True)
+
+        #
+        self.unpool4 = nn.UpsamplingNearest2d(scale_factor=2)
+        
+        self.conv26 = RCR2d(dvgg, 38, 64, 64, 3, 1, 0, True, True)
+        
+        self.conv27 = RCR2d(dvgg, 41, 64, 3, 3, 1, 0, True, False)
+
+    def forward(self, x):
+        x = self.conv15(x)
+        
+        x = self.unpool1(x)
+
+        x = self.conv16(x)
+        x = self.conv17(x)
+        x = self.conv18(x)
+        x = self.conv19(x)
+
+        x = self.unpool2(x)
+
+        x = self.conv20(x)
+        x = self.conv21(x)
+        x = self.conv22(x)
+        x = self.conv23(x)
+
+        x = self.unpool3(x)
+
+        x = self.conv24(x)
+        x = self.conv25(x)
+
+        x = self.unpool4(x)
+
+        x = self.conv26(x)
+        x = self.conv27(x)
+
+        return x
